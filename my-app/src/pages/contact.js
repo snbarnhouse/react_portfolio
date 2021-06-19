29cd8f6acc7078b2104ad41e8fabbcc4d19e3a0b
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-06-18 21:49:07 -0400

    New code for contact page.

--- my-app/src/pages/contact.js
+++ my-app/src/pages/contact.js
@@ -20,21 +20,20 @@ const Contact = () => {
                 <blockquote className="blockquote mb-0">
                             <div className="input-group">
                         <div className="input-group-prepend">
-                                    <span className="input-group-text">First and last name</span>
+                                    <span className="input-group-text">Name:</span>
                                 </div>
                                 <input type="text" aria-label="First name" className="form-control" />
-                                    <input type="text" aria-label="Last name" className="form-control" />
                     </div>
                     <div className="input-group mb-3">
                                         <div className="input-group-prepend">
-                                            <span className="input-group-text" id="inputGroup-sizing-default">Email</span>
+                                            <span className="input-group-text" id="inputGroup-sizing-default">Email:</span>
                                         </div>
                                         <input type="text" className="form-control" aria-label="Sizing example input"
                                             aria-describedby="inputGroup-sizing-default" />
                     </div>
                     <div className="input-group mb-3">
                                             <div className="input-group-prepend">
-                                                <span className="input-group-text" id="inputGroup-sizing-default">Message</span>
+                                                <span className="input-group-text" id="inputGroup-sizing-default">Message:</span>
                                             </div>
                                             <textarea name="input" id="input" cols="30" rows="5"></textarea>
                                         </div>
