22fe2b7c2953a7289a7d3c51c17b7a218bf2f227
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-06-18 21:46:17 -0400

    Adding to contact pages.

--- my-app/src/pages/contact.js
+++ my-app/src/pages/contact.js
@@ -16,6 +16,30 @@ const Contact = () => {
                 <p>Linkedin: <a href="https://www.linkedin.com/in/stephanie-barnhouse-0b6965a7/" target ="_blank">https://www.linkedin.com/in/stephanie-barnhouse-0b6965a7/</a> </p>
                 <p>Resume: <a href="https://docs.google.com/document/d/16y3QFnPuZsyi8BuAgHWPJtYT64PJ6ZoS7XmnsUnMfVk/edit?usp=sharing" target= "_blank">My Resume</a></p>
                 <p>Email: snbarnhouse@yahoo.com</p>
+                <br></br>
+                <blockquote className="blockquote mb-0">
+                            <div className="input-group">
+                        <div className="input-group-prepend">
+                                    <span className="input-group-text">First and last name</span>
+                                </div>
+                                <input type="text" aria-label="First name" className="form-control" />
+                                    <input type="text" aria-label="Last name" className="form-control" />
+                    </div>
+                    <div className="input-group mb-3">
+                                        <div className="input-group-prepend">
+                                            <span className="input-group-text" id="inputGroup-sizing-default">Email</span>
+                                        </div>
+                                        <input type="text" className="form-control" aria-label="Sizing example input"
+                                            aria-describedby="inputGroup-sizing-default" />
+                    </div>
+                    <div className="input-group mb-3">
+                                            <div className="input-group-prepend">
+                                                <span className="input-group-text" id="inputGroup-sizing-default">Message</span>
+                                            </div>
+                                            <textarea name="input" id="input" cols="30" rows="5"></textarea>
+                                        </div>
+                                        <input className="btn btn-primary" type="submit" value="Submit" />
+                </blockquote>
                         
                                     </div>
         </div>
