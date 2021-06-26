47137f3065285031eb677725721fc4b1df3886ae
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-06-26 14:52:32 -0400

    Fixing pathways adding hash browser.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import './App.css';
 import Wrapper from "./components/wrapper/wrapper"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import { HashRouter as Router } from 'react-router-dom'
 import Nav from "./components/navbar/navbar"
 import About from "./pages/about"
 import Portfolio from "./pages/portfolio"
 import Contact from "./pages/contact"
 import NotFoundPage from "./pages/notFoundPage"
+
 function App() {
 
   return (
-    <Router>
+    <Router basename="/">
       <Nav />
       <Wrapper>
         <Switch>
-          <Route path="/react_portfolio" component={About} />
-          <Route exact path="/About" component={About} />
-          <Route exact path="/Portfolio" component={Portfolio} />
-          <Route exact path="/Contact" component={Contact} />
+          <Route exact path="/" component={About} />
+          <Route path="/About" component={About} />
+          <Route path="/Portfolio" component={Portfolio} />
+          <Route path="/Contact" component={Contact} />
           <Route component={NotFoundPage} />        </Switch>
       </Wrapper>
     </Router>
